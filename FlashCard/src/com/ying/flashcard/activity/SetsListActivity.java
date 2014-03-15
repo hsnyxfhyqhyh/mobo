f1056aa2bc6d69c3f906fd291accb65bbe947601
hsnyxfhyqhyh <xfyang46@gmail.com>
2014-03-15 07:24:02 -0400

    can add set

--- FlashCard/src/com/ying/flashcard/activity/SetsListActivity.java
+++ FlashCard/src/com/ying/flashcard/activity/SetsListActivity.java
@@ -16,17 +16,26 @@ import com.ying.flashcard.fragment.FlashcardSetListFragment;
 import android.os.Bundle;
 import android.os.Environment;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 
 public class SetsListActivity extends FragmentActivity implements
 		FlashcardSetListFragment.OnChoiceSelectedListener {
 
+	final Context context = this;
+	
 	public SetHandler setHandler;
+	EditText txtAddSet ;
 
 	MainActivityPreferences preferences = ApplicationInitializeActivity.preferences;
 	private ArrayList<SetDTO> sets;
@@ -35,25 +44,27 @@ public class SetsListActivity extends FragmentActivity implements
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.article_view);
 
-		if (findViewById(R.id.fragment_container) != null) {
-
-			if (savedInstanceState != null) {
-				return;
-			}
-
-			FlashcardSetListFragment firstFragment = new FlashcardSetListFragment();
-
-			firstFragment.setArguments(getIntent().getExtras());
-
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.fragment_container, firstFragment).commit();
-		}
+		displayList();
 	}
 
 	
+	private void displayList() {
+		FrameLayout container = (FrameLayout)findViewById(R.id.fragment_container);
+		if ( container != null) {
+			container.removeAllViews();
+		}
+		
+		FlashcardSetListFragment firstFragment = new FlashcardSetListFragment();
+
+		firstFragment.setArguments(getIntent().getExtras());
+
+		getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
+	}
+
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.menu_set_list, menu);
 		return true;
 	}
 
@@ -92,8 +103,11 @@ public class SetsListActivity extends FragmentActivity implements
 
 		switch (item.getItemId()) {
 
-		case R.id.export_db:
+		case R.id.menu_export_db:
 			exportDB();
+			break;
+		case R.id.menu_add_set:
+			showDialog();
 			break;
 		case R.id.action_settings:
 			break;
@@ -178,4 +192,37 @@ public class SetsListActivity extends FragmentActivity implements
 	public void OnChoiceSelected(int position) {
 
 	}
+	
+	public void showDialog() {
+		// custom dialog
+		final Dialog dialog = new Dialog(context);
+		dialog.setContentView(R.layout.dialog_set_add);
+		dialog.setTitle(R.string.dialog_set_add);
+
+		// set the custom dialog components - text, image and button
+		txtAddSet = (EditText) dialog.findViewById(R.id.txtAddSet);
+		
+		Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+		// if button is clicked, close the custom dialog
+		dialogButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if(!txtAddSet.getText().toString().trim().equals("")){
+					setHandler = new SetHandler(context);
+					SetDTO dto =new SetDTO();
+					dto.setName(txtAddSet.getText().toString());
+					setHandler.create(dto);
+					
+					Toast.makeText(getBaseContext(), txtAddSet.getText() + " 已添加" , Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(getBaseContext(), "请添加正确类名" , Toast.LENGTH_SHORT).show();
+				}
+				
+				displayList();
+				dialog.dismiss();
+			}
+		});
+
+		dialog.show();
+	 }
 }
